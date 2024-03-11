2571cb270e0a8600154c8c8e287f058426715210
Akhil7205 <akhilshinde13401@gmail.com>
2024-03-11 23:38:52 +0530

    mod

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".rb").addEventListener('click', myfun)
+function myfun(){
+ document.querySelector(".rb").innerHTML='clicked'
+    
+}
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+console.log('sdf')
