4f64c9a480c0839bc1163bd34df2fa329d8f3847
Akhil7205 <akhilshinde13401@gmail.com>
2024-03-17 23:23:03 +0530

    modified

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-document.querySelector(".rb").addEventListener('click', myfun)
-function myfun(){
- document.querySelector(".rb").innerHTML='clicked'
-    
-}
+const displayRules = document.querySelector('.display-rules')
+const redCross=document.querySelector('.red-cross')
+const rb = document.querySelector('.rb')
+const nb =document.querySelector('.nb')
+const pa =document.querySelector('.pa-btn')
+const center=document.querySelector('.center-content')
+const firstFrame=document.querySelector('.firstFrame')
 
-document.add
+
+pa.addEventListener('click',()=>{
+  center.style.display='none'
+  firstFrame.style.display='flex'
+})
+
+rb.addEventListener('click', ()=>{
+  displayRules.style.display='block'
+})
+
+redCross.addEventListener('click',()=>{
+  displayRules.style.display='none'
+  
+})
+
+// nb.addEventListener('click',()=>{
+//   body.style.display=
+// })
+
+
+document.getElementById('demo').addEventListener('click', function() {
+  var contentDiv = document.getElementById('body');
+  contentDiv.innerStyle = `
+    
+  `;
+});
